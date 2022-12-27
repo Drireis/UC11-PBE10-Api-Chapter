884a40cd09d81a71e97b393f2347ec1a4547b752
Drireis <adriano.jesus@iesb.edu.br>
2022-12-27 14:43:16 -0300

    Api Chapter UC11

---
+++ Chapter.WebApi/Chapter.WebApi/Contexts/ChapterContext.cs
@@ -0,0 +1,28 @@
+﻿using Chapter.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.WebApi.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext>
+        options)
+        : base(options)
+        {
+        }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+            optionsBuilder.UseSqlServer("DataSource = DESKTOP-ADRIANO\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }                                                                                             //;Id User: sa; pwd=********* (senha)
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ Chapter.WebApi/Chapter.WebApi/Models/Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace Chapter.WebApi.Models
+{
+    // models: armazena as propriedades da classe
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? QuantidadePaginas { get; set; }
+        public bool Disponivel { get; set; }
+
+    }
+}
